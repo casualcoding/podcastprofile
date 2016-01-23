884c681ef0b73a28731b9ab49b9e5bb46f138115
Max Friedrich <hallo@maxfriedrich.de>
2016-01-23 22:12:31 +0100

    Add fallback for itunes:image, cleanup

--- app/Http/Controllers/StaticController.php
+++ app/Http/Controllers/StaticController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 
 class StaticController extends Controller
 {
-
     public function getIndex()
     {
         return view('index');
@@ -35,39 +34,43 @@ class StaticController extends Controller
         }
 
         // TODO render view or redirect
-        return "<br>DONE";
+        return '<br>DONE';
     }
-    
+
     public function testFeed()
     {
-        $json = file_get_contents('https://itunes.apple.com/search?media=podcast&term=tech&limit=50');
+        $json = file_get_contents('https://itunes.apple.com/search?media=podcast&term=life&limit=50');
         $podcasts = json_decode($json);
 
         $result = '';
 
-        foreach($podcasts->results as $podcast) {
+        foreach ($podcasts->results as $podcast) {
             $url = $podcast->feedUrl;
-        
+
             $feed = new SimplePie();
-            $feed->set_cache_location(__DIR__ . '/../../../cache');
+            $feed->set_cache_location(__DIR__.'/../../../cache');
             $feed->set_feed_url($url);
             $feed->init();
 
             $title = $feed->get_title();
             $link = $feed->get_link();
             $summary = $feed->get_channel_tags(SIMPLEPIE_NAMESPACE_ITUNES, 'summary')[0]['data'];
+
+            // <itunes:image href=...>
             $image = $feed->get_channel_tags(SIMPLEPIE_NAMESPACE_ITUNES, 'image')[0]['attribs']['']['href'];
+            if ($image == null) {
+                // <image><url>...</url></image>
+                $image = $feed->get_channel_tags('', 'image')[0]['child']['']['url'][0]['data'];
+            }
 
             $result .= '<table>';
-            $result .= '<tr><td><img src="' . $image . '" width=200 height=200></td>';
-            $result .= '<td><h2>' . $title . '</h2>';
-            $result .= '<p>' . $url . '</p><p>' . $link . '</p><p>' . $summary . '</p>';
+            $result .= '<tr><td><img src="'.$image.'" width=200 height=200></td>';
+            $result .= '<td><h2>'.$title.'</h2>';
+            $result .= '<p>'.$url.'</p><p>'.$link.'</p><p>'.$summary.'</p>';
             $result .= '</td></tr>';
             $result .= '</table>';
         }
-        
+
         return $result;
     }
-
-
 }
