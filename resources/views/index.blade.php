86d8eb8b57c2e2166cf1b6ea48b9a520053316e1
Florian Letsch <florian@yootheme.com>
2016-01-23 16:53:25 +0100

    created static views

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,18 @@
-<!-- View stored in resources/views/greeting.php -->
+@extends('layouts.master')
+@section('title', 'Show what podcasts you listen to')
 
-<html>
-    <body>
+@section('content')
+
         <h1>Hello.</h1>
-    </body>
-</html>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat.
+        </p>
+
+        <a href="/">Sign in with Twitter</a>
+
+@stop
--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,18 +1,23 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-    </head>
-    <body>
-        <div class="">
-            <h2>Upload XML file</h2>
+@extends('layouts.master')
+@section('title', 'User Profile')
 
-            <form action="/upload" method="post">
-                <input type="file" name="upload" value="">
-                <input type="submit">
-            </form>
+@section('content')
 
-        </div>
-    </body>
-</html>
+    <h1>Your Profile Settings</h1>
+
+    <div class="uk-panel uk-panel-box">
+
+        <h2>Upload XML file</h2>
+
+        <form action="/upload" method="post">
+            <input type="file" name="upload" value="">
+            <input type="submit">
+        </form>
+
+    </div>
+
+    <ul>
+
+    </ul>
+
+@stop
