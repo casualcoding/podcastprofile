f6f082c072e8c7496bb5763047f13334647be2f2
Florian Letsch <florian@yootheme.com>
2016-01-23 15:57:22 +0100

    Added static views

---
+++ resources/views/settings.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <div class="">
+            <h2>Upload XML file</h2>
+
+            <form action="/upload" method="post">
+                <input type="file" name="upload" value="">
+                <input type="submit">
+            </form>
+
+        </div>
+    </body>
+</html>
