30ae1f44293ffe7850b6bf1b94baf855e197a78d
Max Friedrich <hallo@maxfriedrich.de>
2016-01-24 13:02:56 +0100

    Add favicon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title') | Podcast Profile</title>
-        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
         <link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon.png">
         <link rel="stylesheet" href="/assets/dist/app.css" media="screen">
         <script src="/assets/dist/app.js"></script>
