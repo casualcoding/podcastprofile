dacb766b77604672c7afe8d490f4ba3829af9b4f
Max Friedrich <hallo@maxfriedrich.de>
2016-01-24 20:39:28 +0100

    add 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.navbar')
+
+@section('title', '404')
+
+@section('body-classes')
+
+@section('content')
+
+    <div class="uk-container uk-container-center uk-margin-large-top uk-text-center">
+        <img src="/assets/logo-gray@2x.png" width="400" height="400" alt="">
+        <h2>404 Pustekuchen</h2>
+    </div>
+
+@stop
