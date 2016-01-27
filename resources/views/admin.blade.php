8410dcbdafc7cfa91d5a40db795b8c248ef771ff
Florian Letsch <florian@yootheme.com>
2016-01-27 23:03:21 +0100

    made table resizable. at least to a certain point

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -17,16 +17,16 @@
         <ul id="lists" class="uk-switcher uk-margin">
             <li>
                 <div class="uk-overflow-container">
-                    <table class="uk-table uk-table-striped">
+                    <table class="uk-table uk-table-striped" style="table-layout: fixed;">
                         <thead>
                             <tr>
-                                <th>handle</th>
-                                <th>name</th>
-                                <th>url</th>
-                                <th>twitter_id</th>
-                                <th>role</th>
-                                <th>created_at</th>
-                                <th>updated_at</th>
+                                <th class="uk-width-2-10">handle</th>
+                                <th class="uk-width-2-10">name</th>
+                                <th class="uk-width-2-10">url</th>
+                                <th class="uk-width-1-10">twitter id</th>
+                                <th class="uk-width-1-10 uk-text-right">role</th>
+                                <th class="uk-width-1-10">created</th>
+                                <th class="uk-width-1-10">updated</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -34,11 +34,11 @@
                             <tr>
                                 <td><a href="/{{ $user->handle }}">{{ $user->handle }}</a></td>
                                 <td>{{ $user->name }}</td>
-                                <td class="uk-width-1-10 uk-text-truncate"><a href="/{{ $user->url }}">{{ $user->url }}</a></td>
+                                <td><div class="uk-width-1-1 uk-text-truncate"><a href="/{{ $user->url }}">{{ $user->url }}</a></div></td>
                                 <td>{{ $user->twitter_id }}</td>
-                                <td>{{ $user->role }}</td>
-                                <td>{{ $user->created_at }}</td>
-                                <td>{{ $user->updated_at }}</td>
+                                <td class="uk-text-right">{{ $user->role }}</td>
+                                <td class="uk-text-small uk-text-muted">{{ $user->created_at }}</td>
+                                <td class="uk-text-small uk-text-muted">{{ $user->updated_at }}</td>
                             </tr>
                             @endforeach
                         </tbody>
@@ -46,47 +46,49 @@
                 </div>
             </li>
             <li>
-                <div class="uk-overflow-container">
-                    <table class="uk-table uk-table-striped">
-                        <thead>
-                            <tr>
-                                <th>name</th>
-                                <th>url</th>
-                                <th>feed</th>
-                                <th>coverimage</th>
-                                <!-- <th>description</th> -->
-                                <th>error</th>
-                                <th>created_at</th>
-                                <th>updated_at</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($podcasts as $podcast)
-                            <tr>
-                                @if ($podcast->name == null)
-                                <td class="uk-text-danger">None</td>
-                                @else
-                                <td>{{ $podcast->name }}</td>
-                                @endif
-                                
-                                @if ($podcast->url == null)
-                                <td class="uk-text-danger">None</td>
-                                @else
-                                <td><a href="{{ $podcast->url }}">{{ $podcast->url }}</a></td>
-                                @endif
-                                
-                                <td class="uk-width-1-10"><a href="{{ $podcast->feed }}">{{ $podcast->feed }}</a></td>
-                                
-                                <td><a href="/{{ $podcast->coverimage }}"><img src="{{ $podcast->coverimage }}" width="60" height="60"></a></td>
-                                <!-- <td>{{ $podcast->description }}</td> -->
-                                <td @if ($podcast->error != 0) class="uk-text-danger" @endif>{{ $podcast->error }}</td>
-                                <td>{{ $podcast->created_at }}</td>
-                                <td>{{ $podcast->updated_at }}</td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                <table class="uk-table uk-width-1-1 uk-table-striped" style="table-layout: fixed;">
+                    <thead>
+                        <tr>
+                            <th class="uk-width-2-10">name</th>
+                            <th class="uk-width-4-10">url / feed</th>
+                            <th class="uk-width-1-10">cover</th>
+                            <!-- <th>description</th> -->
+                            <th class="uk-width-1-10">error</th>
+                            <th class="uk-width-1-10">created</th>
+                            <th class="uk-width-1-10">updated</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($podcasts as $podcast)
+                        <tr>
+                            @if ($podcast->name == null)
+                            <td class="uk-text-danger">None</td>
+                            @else
+                            <td >{{ $podcast->name }}</td>
+                            @endif
+
+                            <td>
+                                <div class="uk-width-1-1 uk-text-truncate">
+                                    <!-- url -->
+                                    @if ($podcast->url == null)
+                                    <span class="uk-text-danger">None</span>
+                                    @else
+                                    <i class="uk-icon-link"></i> <a href="{{ $podcast->url }}">{{ $podcast->url }}</a>
+                                    @endif
+
+                                    <!-- feed -->
+                                    <br><i class="uk-icon-rss"></i> <a href="{{ $podcast->feed }}">{{ $podcast->feed }}</a>
+                                </div>
+                            </td>
+                            <td><a href="/{{ $podcast->coverimage }}"><img src="{{ $podcast->coverimage }}" width="60" height="60"></a></td>
+                            <!-- <td>{{ $podcast->description }}</td> -->
+                            <td  @if ($podcast->error != 0) class="uk-text-danger" @endif>{{ $podcast->error }}</td>
+                            <td class="uk-text-small uk-text-muted">{{ $podcast->created_at }}</td>
+                            <td class="uk-text-small uk-text-muted">{{ $podcast->updated_at }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </li>
             <li><pre>{{ var_dump($jobs) }}</pre></li>
             <li><pre>{{ var_dump($failed_jobs) }}</pre></li>
