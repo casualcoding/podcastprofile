7b006db19ddbc136bb6f0038c707859a31329599
Florian Letsch <florian@yootheme.com>
2016-01-27 23:14:18 +0100

    added numbers. everyone loves numbers.

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -7,10 +7,10 @@
     <div id="admin">
         <!-- This is the tabbed navigation containing the toggling elements -->
         <ul class="uk-tab uk-margin-large-top" data-uk-tab="{connect:'#lists'}">
-            <li><a href="">Users</a></li>
-            <li><a href="">Podcasts</a></li>
-            <li><a href="">Jobs</a></li>
-            <li><a href="">Failed Jobs</a></li>
+            <li><a href="">{{ count($users) }} users</a></li>
+            <li><a href="">{{ count($podcasts) }} podcasts</a></li>
+            <li><a href="">{{ count($jobs) }} jobs</a></li>
+            <li><a href="">{{ count($failed_jobs) }} failed Jobs</a></li>
         </ul>
 
         <!-- This is the container of the content items -->
@@ -46,49 +46,52 @@
                 </div>
             </li>
             <li>
-                <table class="uk-table uk-width-1-1 uk-table-striped" style="table-layout: fixed;">
-                    <thead>
-                        <tr>
-                            <th class="uk-width-2-10">name</th>
-                            <th class="uk-width-4-10">url / feed</th>
-                            <th class="uk-width-1-10">cover</th>
-                            <!-- <th>description</th> -->
-                            <th class="uk-width-1-10">error</th>
-                            <th class="uk-width-1-10">created</th>
-                            <th class="uk-width-1-10">updated</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($podcasts as $podcast)
-                        <tr>
-                            @if ($podcast->name == null)
-                            <td class="uk-text-danger">None</td>
-                            @else
-                            <td >{{ $podcast->name }}</td>
-                            @endif
+                <div class="uk-overflow-container">
 
-                            <td>
-                                <div class="uk-width-1-1 uk-text-truncate">
-                                    <!-- url -->
-                                    @if ($podcast->url == null)
-                                    <span class="uk-text-danger">None</span>
-                                    @else
-                                    <i class="uk-icon-link"></i> <a href="{{ $podcast->url }}">{{ $podcast->url }}</a>
-                                    @endif
+                    <table class="uk-table uk-width-1-1 uk-table-striped" style="table-layout: fixed;">
+                        <thead>
+                            <tr>
+                                <th class="uk-width-2-10">name</th>
+                                <th class="uk-width-4-10">url / feed</th>
+                                <th class="uk-width-1-10">cover</th>
+                                <!-- <th>description</th> -->
+                                <th class="uk-width-1-10">error</th>
+                                <th class="uk-width-1-10">created</th>
+                                <th class="uk-width-1-10">updated</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($podcasts as $podcast)
+                            <tr>
+                                @if ($podcast->name == null)
+                                <td class="uk-text-danger">None</td>
+                                @else
+                                <td >{{ $podcast->name }}</td>
+                                @endif
 
-                                    <!-- feed -->
-                                    <br><i class="uk-icon-rss"></i> <a href="{{ $podcast->feed }}">{{ $podcast->feed }}</a>
-                                </div>
-                            </td>
-                            <td><a href="/{{ $podcast->coverimage }}"><img src="{{ $podcast->coverimage }}" width="60" height="60"></a></td>
-                            <!-- <td>{{ $podcast->description }}</td> -->
-                            <td  @if ($podcast->error != 0) class="uk-text-danger" @endif>{{ $podcast->error }}</td>
-                            <td class="uk-text-small uk-text-muted">{{ $podcast->created_at }}</td>
-                            <td class="uk-text-small uk-text-muted">{{ $podcast->updated_at }}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                                <td>
+                                    <div class="uk-width-1-1 uk-text-truncate">
+                                        <!-- url -->
+                                        @if ($podcast->url == null)
+                                        <span class="uk-text-danger">None</span>
+                                        @else
+                                        <i class="uk-icon-link"></i> <a href="{{ $podcast->url }}">{{ $podcast->url }}</a>
+                                        @endif
+
+                                        <!-- feed -->
+                                        <br><i class="uk-icon-rss"></i> <a href="{{ $podcast->feed }}">{{ $podcast->feed }}</a>
+                                    </div>
+                                </td>
+                                <td><a href="/{{ $podcast->coverimage }}"><img src="{{ $podcast->coverimage }}" width="60" height="60"></a></td>
+                                <!-- <td>{{ $podcast->description }}</td> -->
+                                <td  @if ($podcast->error != 0) class="uk-text-danger" @endif>{{ $podcast->error }}</td>
+                                <td class="uk-text-small uk-text-muted">{{ $podcast->created_at }}</td>
+                                <td class="uk-text-small uk-text-muted">{{ $podcast->updated_at }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </li>
             <li><pre>{{ var_dump($jobs) }}</pre></li>
             <li><pre>{{ var_dump($failed_jobs) }}</pre></li>
