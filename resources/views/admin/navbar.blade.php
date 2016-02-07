f663db62bc5919a6cecd3695db08aab8478b7166
Florian Letsch <florian@yootheme.com>
2016-02-07 19:04:37 +0100

    admin nav markup

--- resources/views/admin/navbar.blade.php
+++ resources/views/admin/navbar.blade.php
@@ -1,14 +1,14 @@
-<ul class="uk-subnav">
-    <li @if ($active == 'users') class="active" @endif>
+<ul class="uk-subnav uk-subnav-pill uk-subnav-line">
+    <li @if ($active == 'users') class="uk-active" @endif>
         <a href="{{ URL::route('admin::getUsers') }}">{{ $counts['users'] }} Users</a>
     </li>
-    <li @if ($active == 'podcasts') class="active" @endif>
+    <li @if ($active == 'podcasts') class="uk-active" @endif>
         <a href="{{ URL::route('admin::getPodcasts') }}">{{ $counts['podcasts'] }} Podcasts</a>
     </li>
-    <li @if ($active == 'jobs') class="active" @endif>
+    <li @if ($active == 'jobs') class="uk-active" @endif>
         <a href="{{ URL::route('admin::getJobs') }}">{{ $counts['jobs'] }} Jobs</a>
     </li>
-    <li @if ($active == 'failed_jobs') class="active" @endif>
+    <li @if ($active == 'failed_jobs') class="uk-active" @endif>
         <a href="{{ URL::route('admin::getFailedJobs') }}">{{ $counts['failed_jobs'] }} Failed Jobs</a>
     </li>
 </ul>
